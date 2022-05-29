30348832438bef243ead1eef5530b1d1ee2a43df
Pouya Dehghani <pouyadh.dev@gmail.com>
2022-05-29 23:05:42 +0430

    create Loading.jsx

---
+++ client/src/pages/Loading.jsx
@@ -0,0 +1,34 @@
+import { Container, LinearProgress, Typography } from "@mui/material";
+import React from "react";
+import LogoIcon from "../components/Logo/LogoIcon";
+import LogoText from "../components/Logo/LogoText";
+
+const styles = {
+  container: {
+    textAlign: "center",
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+};
+
+const appVersion = process.env.REACT_APP_VERSION;
+
+const Loading = () => {
+  return (
+    <Container disableGutters sx={styles.container}>
+      <LogoIcon sx={{ fontSize: "128px", mb: 4 }} />
+      <LogoText height="50px" />
+      <LinearProgress
+        sx={{ width: "96px", height: "10px", borderRadius: "10px", mt: 10 }}
+      />
+      <Typography variant="subtitle2" color="grey.600" sx={{ mt: 4 }}>
+        V{appVersion}
+      </Typography>
+    </Container>
+  );
+};
+
+export default Loading;
