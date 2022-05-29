de8e7c5dc9bd9fc0500de2d50192e6db718f8a4e
Pouya Dehghani <pouyadh.dev@gmail.com>
2022-05-30 01:00:50 +0430

    create Register.jsx

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,109 @@
+import { Button, Container, Paper, Stack, Typography } from "@mui/material";
+import * as React from "react";
+import LogoText from "../components/Logo/LogoText";
+
+import Input from "../components/Input/Input";
+import LinearProgress from "../components/Progress/LinearProgress";
+import { Field, Form, Formik } from "formik";
+import * as Yup from "yup";
+
+const initialValues = {
+  username: "",
+  email: "",
+  password: "",
+  cpassword: "",
+};
+
+const validationSchema = Yup.object({
+  username: Yup.string()
+    .min(4, "At least 4 character")
+    .max(50, "Maximum 50 character")
+    .required("Required"),
+  email: Yup.string().email("Should be valid").required("Required"),
+  password: Yup.string()
+    .min(8, "At least 8 character")
+    .max(50, "Maximum 50 character")
+    .required("Required"),
+  cpassword: Yup.string()
+    .oneOf([Yup.ref("password"), null], "Passwords must match")
+    .required("Required"),
+});
+
+const handleSubmit = async (values) => {
+  console.log(values);
+  await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 2000);
+  });
+};
+
+const Register = () => {
+  return (
+    <Container maxWidth="xs" sx={{ pt: "auto" }}>
+      <Paper
+        sx={{
+          textAlign: "center",
+          padding: "auto",
+          p: 4,
+          mt: 4,
+        }}
+        elevation={10}
+      >
+        <Stack direction="column" spacing={2}>
+          <LogoText height="24px" />
+          <Formik
+            initialValues={initialValues}
+            onSubmit={handleSubmit}
+            validationSchema={validationSchema}
+          >
+            {({ submitForm, isSubmitting }) => (
+              <Form>
+                <Stack direction="column" spacing={2}>
+                  <Typography variant="h5" alignSelf="start">
+                    {isSubmitting && <LinearProgress sx={{ height: "5px" }} />}
+                    Register
+                  </Typography>
+                  <Field
+                    component={Input.Username}
+                    type="text"
+                    name="username"
+                    label="Username"
+                  />
+
+                  <Field
+                    component={Input.Email}
+                    type="email"
+                    name="email"
+                    label="Email"
+                  />
+                  <Field
+                    component={Input.Password}
+                    name="password"
+                    label="Password"
+                  />
+                  <Field
+                    component={Input.Password}
+                    name="cpassword"
+                    label="Confirm Password"
+                  />
+
+                  <Button
+                    variant="contained"
+                    disabled={isSubmitting}
+                    onClick={submitForm}
+                  >
+                    Register
+                  </Button>
+                </Stack>
+              </Form>
+            )}
+          </Formik>
+          <Button variant="text">Login</Button>
+        </Stack>
+      </Paper>
+    </Container>
+  );
+};
+
+export default Register;
